89cf4772ccfcf36f374ea3a6fc092e54fa8e8453
Buddhima Zoysa <buddhimazoysa123@gmail.com>
2020-11-23 16:57:04 +0530

    Make this the main Application class

--- Hardware Accelerated 3D Rendering Engine/src/Application.h
+++ Hardware Accelerated 3D Rendering Engine/src/Application.h
@@ -3,87 +3,28 @@
 
 //Local Includes
 #include "EngineBase.h"
+#include "DrawableSystem/Box.h"
+#include "DrawableSystem/BoxWithTexture.h"
+#include "DrawableSystem/Skull.h"
 
 //Standard Includes
 #include <vector>
 #include <cstdint>
 
 class Application : public EngineBase {
-private:
-	Application();
-
 public:
-	static Application& GetSingleton();
+	using EngineBase::EngineBase;
+private:
+	bool OnStart() override; 
+	bool OnUpdate(float dt) override; 
+	bool OnDestroy() override;
 
 private:
-	struct Vector3 {
-		float x = 0.0f;
-		float y = 0.0f;
-		float z = 0.0f;
-	};
-
-	struct Vertex {
-		Vector3 Position;
-	};
-
-private:
-	bool OnStart() override;
-	bool OnUpdate(float dt) override;
-
-private:
-
-	//Vertices of the box
-	std::vector<Vertex> boxVertices = {
-		{-0.5f, -0.5f, -0.5f},
-		{-0.5f,  0.5f, -0.5f},
-		{ 0.5f,  0.5f, -0.5f},
-		{ 0.5f, -0.5f, -0.5f},
-		{ 0.5f, -0.5f,  0.5f},
-		{ 0.5f,  0.5f,  0.5f},
-		{-0.5f,  0.5f,  0.5f},
-		{-0.5f, -0.5f,  0.5f},
-	};
+	Box* pBox = nullptr;
+	Box* pBox2 = nullptr;
+	BoxWithTexture* pBoxTex = nullptr;
+	Skull* pSkull = nullptr;
 	
-	//Indices of the box
-	std::vector<uint16_t> boxIndices = {
-		0,1,2,
-		0,2,3,
-
-		3,2,5,
-		3,5,4,
-
-		4,5,6,
-		4,6,7,
-
-		7,6,1,
-		7,1,0,
-
-		1,6,5,
-		1,5,2,
-
-		7,0,3,
-		7,3,4,
-	};
-
-
-	float fBoxX = 0.0f;
-	float fBoxY = 0.0f;
-	float fBoxZ = 0.0f;
-	float fBoxRoll = 0.0f;
-	float fBoxPitch = 0.0f;
-	float fBoxYaw = 0.0f;
-
-	Microsoft::WRL::ComPtr<ID3D11Buffer> pBoxVertexBuffer;
-	Microsoft::WRL::ComPtr<ID3D11Buffer> pBoxIndexBuffer;
-	Microsoft::WRL::ComPtr<ID3DBlob> pShaderBlob;
-	Microsoft::WRL::ComPtr<ID3D11Buffer> pBoxVertexShaderConstantBuffer;
-	Microsoft::WRL::ComPtr<ID3D11VertexShader> pBoxVertexShader;
-	Microsoft::WRL::ComPtr<ID3D11Buffer> pBoxPixelShaderConstantBuffer;
-	Microsoft::WRL::ComPtr<ID3D11PixelShader> pBoxPixelShader;
-	Microsoft::WRL::ComPtr<ID3D11InputLayout> pBoxInputLayout;
-
-	//Singleton
-	static Application singleton;
 };
 
 #endif // !APPLICATION_H

2f1e5fe4fc9c49f19359aea8093eedc7911c58f9
Buddhima Zoysa <buddhimazoysa123@gmail.com>
2020-11-23 17:00:23 +0530

    Moved the transforms to the Drawable

--- Hardware Accelerated 3D Rendering Engine/src/BindableSystem/TransformVSCBuf.h
+++ Hardware Accelerated 3D Rendering Engine/src/BindableSystem/TransformVSCBuf.h
@@ -4,33 +4,20 @@
 //Local Includes
 #include "Bindable.h"
 #include "VSConstantBuffer.h"
+#include "../DrawableSystem/Drawable.h"
 
 //DirectX Includes
 #include <DirectXMath.h>
 
 class TransformVSCBuf : public Bindable {
 public:
-	TransformVSCBuf(const Renderer* pRenderer, const DirectX::XMFLOAT3& position = {}, const DirectX::XMFLOAT3& rotation = {});
+	TransformVSCBuf(const Renderer* pRenderer, const Drawable* pParent);
 
 	virtual void Bind() const override;
-
-	void SetX(const float& x);
-	void SetY(const float& y);
-
-	void SetZ(const float& z);
-
-	void SetPosition(const DirectX::XMFLOAT3& position);
-
-	void SetPitch(const float& pitch);
-	void SetYaw(const float& yaw);
-	void SetRoll(const float& roll);
-	void SetRotation(const DirectX::XMFLOAT3& rotation);
-
 private:
-	mutable VSConstantBuffer<DirectX::XMMATRIX> m_vsConstantBuf;
-	DirectX::XMFLOAT3 m_f3Position = {};
-	DirectX::XMFLOAT3 m_f3Rotation = {};
-	DirectX::XMMATRIX m_mxmatProjectionMatrix = {};
+	const Renderer* m_pRenderer = nullptr;
+	const Drawable* m_pParentDrawable = nullptr;
+	mutable VSConstantBuffer<DirectX::XMMATRIX> m_vscTransformVSConstantBuf;
 };
 
 #endif // !TRANSFORMVSCBUF_H

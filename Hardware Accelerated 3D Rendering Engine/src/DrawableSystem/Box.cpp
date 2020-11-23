8aba4f9122321cf1b424f214db7f11f9cf75dbf0
Buddhima Zoysa <buddhimazoysa123@gmail.com>
2020-11-23 17:00:44 +0530

    Box Drawable

--- Hardware Accelerated 3D Rendering Engine/src/DrawableSystem/Box.cpp
+++ Hardware Accelerated 3D Rendering Engine/src/DrawableSystem/Box.cpp
@@ -1,16 +1,33 @@
 #include "Box.h"
 
+
+
 Box::Box(const Renderer* pRenderer) : Drawable(pRenderer) {
 	AddBindable(new VertexBuffer(GetRenderer(), (UINT)m_vecVertices.size(), (UINT)sizeof(m_vecVertices[0]), m_vecVertices.data()));
 	AddIndexBufferBindable(new IndexBuffer(GetRenderer(), (UINT)m_vecIndices.size(), (UINT)sizeof(m_vecIndices[0]), DXGI_FORMAT::DXGI_FORMAT_R16_UINT, m_vecIndices.data()));
-	const VertexShader* pVertexShader = new VertexShader(GetRenderer(), std::wstring(L"Shaders/VertexShaderBox.cso"));
+	VertexShader* pVertexShader = new VertexShader(GetRenderer(), std::wstring(L"Shaders/VertexShaderBox.cso"));
 	AddBindable(pVertexShader);
-	AddBindable(new PixelShader(GetRenderer(), std::wstring(L"Shaders/BoxPixelShader.cso")));
+	AddBindable(new PixelShader(GetRenderer(), std::wstring(L"Shaders/PixelShaderBox.cso")));
 	std::vector<D3D11_INPUT_ELEMENT_DESC> BoxInputLayouts = { { "POSITION", 0u, DXGI_FORMAT::DXGI_FORMAT_R32G32B32_FLOAT, 0u, 0u, D3D11_INPUT_CLASSIFICATION::D3D11_INPUT_PER_VERTEX_DATA, 0u }, };
 	AddBindable(new InputLayout(GetRenderer(), BoxInputLayouts, *pVertexShader));
 	AddBindable(new PrimitiveTopology(GetRenderer(), D3D11_PRIMITIVE_TOPOLOGY::D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST));
+	AddBindable(new TransformVSCBuf(GetRenderer(), this));
 
-	vcBuf = new TransformVSCBuf(GetRenderer(), DirectX::XMFLOAT3(0.0f, 0.0f, 3.0f));
-	AddBindable(vcBuf);
+	AddBindable(new PSConstantBuffer<PSConstBuf>(GetRenderer(), m_colors));
 
+}
+
+const void Box::SetPosition(const DirectX::XMFLOAT3& Position)
+{
+	m_Position = Position;
+}
+
+const void Box::SetRotation(const DirectX::XMFLOAT3& Rotation)
+{
+	m_Rotation = Rotation;
+}
+
+const DirectX::XMMATRIX Box::GetTransform() const {
+	using namespace DirectX;
+	return XMMatrixRotationRollPitchYaw(m_Rotation.x, m_Rotation.y, m_Rotation.z)* XMMatrixTranslation(m_Position.x, m_Position.y, m_Position.z);
 }
--- Hardware Accelerated 3D Rendering Engine/src/DrawableSystem/Box.h
+++ Hardware Accelerated 3D Rendering Engine/src/DrawableSystem/Box.h
@@ -16,44 +16,53 @@ struct Vertex {
 };
 
 class Box : public Drawable {
+	struct PSConstBuf {
+		DirectX::XMFLOAT4 colors[6];
+	};
+
 public:
 	Box(const Renderer* pRenderer);
+	const void SetPosition(const DirectX::XMFLOAT3& Position);
+	const void SetRotation(const DirectX::XMFLOAT3& Rotation);
+	constexpr DirectX::XMFLOAT3& GetRotation() { return m_Rotation; }
+	constexpr DirectX::XMFLOAT3& GetPosition() { return m_Position; }
+	virtual const DirectX::XMMATRIX GetTransform() const override;
 
 private:
-	//const std::vector<Vertex> m_vecVertices = {
-	//	{DirectX::XMFLOAT3(-1.0f, -1.0f, -1.0f)},
-	//	{DirectX::XMFLOAT3( 1.0f, -1.0f, -1.0f)},
-	//	{DirectX::XMFLOAT3(-1.0f,  1.0f, -1.0f)},
-	//	{DirectX::XMFLOAT3( 1.0f,  1.0f, -1.0f)},
-	//	{DirectX::XMFLOAT3(-1.0f, -1.0f,  1.0f)},
-	//	{DirectX::XMFLOAT3( 1.0f, -1.0f,  1.0f)},
-	//	{DirectX::XMFLOAT3(-1.0f,  1.0f,  1.0f)},
-	//	{DirectX::XMFLOAT3( 1.0f,  1.0f,  1.0f)},
-	//};
-
-	//const std::vector<uint16_t> m_vecIndices = {
-	//	0,2,1, 2,3,1,
-	//	1,3,5, 3,7,5,
-	//	2,6,3, 3,6,7,
-	//	4,5,7, 4,7,6,
-	//	0,4,2, 2,4,6,
-	//	0,1,4, 1,5,4,
-	//};
-
-
-	std::vector<Vertex> m_vecVertices = {
-	{DirectX::XMFLOAT3(-0.75f, -0.75f, 0.0f)},
-	{DirectX::XMFLOAT3(0.0f, 0.75f, 0.0f)},
-	{DirectX::XMFLOAT3(0.75f, -0.75f, 0.0f)},
-	{DirectX::XMFLOAT3(-1.0f, 1.0f, 0.0f)},
+	const std::vector<Vertex> m_vecVertices = {
+		{DirectX::XMFLOAT3(-1.0f, -1.0f, -1.0f)},
+		{DirectX::XMFLOAT3( 1.0f, -1.0f, -1.0f)},
+		{DirectX::XMFLOAT3(-1.0f,  1.0f, -1.0f)},
+		{DirectX::XMFLOAT3( 1.0f,  1.0f, -1.0f)},
+		{DirectX::XMFLOAT3(-1.0f, -1.0f,  1.0f)},
+		{DirectX::XMFLOAT3( 1.0f, -1.0f,  1.0f)},
+		{DirectX::XMFLOAT3(-1.0f,  1.0f,  1.0f)},
+		{DirectX::XMFLOAT3( 1.0f,  1.0f,  1.0f)},
 	};
 
-	std::vector<uint16_t> m_vecIndices = {
-		0, 1, 2,
-		0, 3, 1,
+	const std::vector<uint16_t> m_vecIndices = {
+		0,2,1, 2,3,1,
+		1,3,5, 3,7,5,
+		2,6,3, 3,6,7,
+		4,5,7, 4,7,6,
+		0,4,2, 2,4,6,
+		0,1,4, 1,5,4,
 	};
 
-	const TransformVSCBuf* vcBuf = nullptr;
+	const PSConstBuf m_colors = { 
+		{
+		DirectX::XMFLOAT4(1.0f, 1.0f, 1.0f, 1.0f),
+		DirectX::XMFLOAT4(0.0f, 0.0f, 1.0f, 1.0f),
+		DirectX::XMFLOAT4(0.0f, 1.0f, 0.0f, 1.0f),
+		DirectX::XMFLOAT4(0.0f, 1.0f, 1.0f, 1.0f),
+		DirectX::XMFLOAT4(1.0f, 0.0f, 0.0f, 1.0f),
+		DirectX::XMFLOAT4(1.0f, 0.0f, 1.0f, 1.0f),
+		}
+	};
+
+	DirectX::XMFLOAT3 m_Position = DirectX::XMFLOAT3(0.0f, 0.0f, 4.0f);
+	DirectX::XMFLOAT3 m_Rotation = DirectX::XMFLOAT3(0.0f, 0.0f, 0.0f);
+	
 };
 
 #endif // !BOX_H
